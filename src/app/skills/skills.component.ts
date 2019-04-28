cd8376b1fa50340bd8105cfc0e2d820dc595c772
jessestrode <jessedans91@gmail.com>
2019-04-28 10:40:46 -0400

    add skills; add styling

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
+  public skills: Array<{ name: string, rating: number }> = [
+    { name: 'JAVA', rating: 90 },
+    { name: 'HTML', rating: 95 },
+    { name: 'SPRING BOOT', rating: 75 },
+    { name: 'SQL', rating: 85 },
+    { name: 'CSS', rating: 70 },
+    { name: 'HADOOP', rating: 30 },
+  ];
   constructor() { }
 
   ngOnInit() {
